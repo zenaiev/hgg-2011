460888b0f34062043c805aebe2f8fae8b794f8fc
Oleksandr Zenaiev <oleksandr.zenaiev@gmail.com>
2016-12-12 19:45:34 +0100

    modified compile

--- PostAnalyzer/compile.sh
+++ PostAnalyzer/compile.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# compile code (produces two executables)
 g++ -o ttbarReco `root-config --cflags --libs` -lMathMore -std=c++11 ttbarReco.cxx
 g++ -o ttbarPlots `root-config --cflags --libs` -std=c++11 ttbarPlots.cxx
+
+# create needed directories if do not exist yet
+mkdir -p data mc hist plots
