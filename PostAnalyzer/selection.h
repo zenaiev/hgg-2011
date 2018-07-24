0b4d849d22788fc31e25d5e95841a992bfacaced
Christian Staufenbiel <c.staufe@posteo.net>
2018-07-24 15:18:14 +0200

    Added 2012 PhotonID selection (missing Isolation variables)

--- PostAnalyzer/selection.h
+++ PostAnalyzer/selection.h
@@ -38,7 +38,7 @@ int PhotonClass(const double eta, const double r9)
 //   const int el: muon candidate
 // Returns true for selected muon, false otherwise.
 // See tree.h for ZTree variables description.
-double SelectPh(const int eventClass, const ZTree* preselTree, const int ph)
+double SelectPh11(const int eventClass, const ZTree* preselTree, const int ph)
 {
   // relative combined isolation using selected event vertex 5.4.1
   if(gFlagDebug) printf("5.4.1\n");
@@ -97,6 +97,65 @@ double SelectPh(const int eventClass, const ZTree* preselTree, const int ph)
   return 1;
 }
 
+double SelectPh12(const int eventClass, const ZTree* preselTree, const int ph)
+{
+  // relative combined isolation using selected event vertex 5.4.1
+  if(gFlagDebug) printf("5.4.1\n");
+  const double aEff = 0.17;
+  double relCombIso = preselTree->phTrkSumPtHollowConeDR03[ph] + preselTree->phEcalRecHitSumEtConeDR03[ph] + preselTree->phHcalTowerSumEtConeDR04[ph];
+  //double relCombIso = preselTree->phTrkSumPtHollowConeDR03[ph] + preselTree->phEcalRecHitSumEtConeDR03[ph] + preselTree->phHcalTowerSumEtConeDR03[ph];
+  relCombIso -= aEff * preselTree->rho;
+  relCombIso /= (TMath::Abs(preselTree->phPt[ph]) / 50.0);
+  if( (relCombIso > 3.8 && eventClass == 3) ||
+      (relCombIso > 2.2 && eventClass == 4) ||
+      (relCombIso > 1.77 && eventClass == 5) ||
+      (relCombIso > 1.29 && eventClass == 6) )
+    return 0;
+
+  // relative track isolation using selected event vertex 5.4.3
+  if(gFlagDebug) printf("5.4.3\n");
+  double relTrackIso = preselTree->phTrkSumPtHollowConeDR03[ph];
+  relTrackIso /= (TMath::Abs(preselTree->phPt[ph]) / 50.0);
+  if( (relTrackIso > 3.5 && eventClass == 3) ||
+      (relTrackIso > 2.2 && eventClass == 4) ||
+      (relTrackIso > 2.3 && eventClass == 5) ||
+      (relTrackIso > 1.45 && eventClass == 6) )
+    return 0;
+
+  // H/E 5.4.4
+  if(gFlagDebug) printf("5.4.4\n");
+  if( (preselTree->phHadronicOverEm[ph] > 0.124 && eventClass == 3) ||
+      (preselTree->phHadronicOverEm[ph] > 0.092 && eventClass == 4) ||
+      (preselTree->phHadronicOverEm[ph] > 0.142 && eventClass == 5) ||
+      (preselTree->phHadronicOverEm[ph] > 0.063 && eventClass == 6) )
+    return 0;
+
+  // covietaieta 5.4.5
+  if(gFlagDebug) printf("5.4.5\n");
+  double sigmaIEtaIEta = preselTree->phSigmaIetaIeta[ph] * preselTree->phSigmaIetaIeta[ph];
+  if( (sigmaIEtaIEta > 0.0108 && eventClass == 3) ||
+      (sigmaIEtaIEta > 0.0102 && eventClass == 4) ||
+      (sigmaIEtaIEta > 0.028 && eventClass == 5) ||
+      (sigmaIEtaIEta > 0.028 && eventClass == 6) )
+    return 0;
+
+  // r9 5.4.6
+  if(gFlagDebug) printf("5.4.6\n");
+  if( (preselTree->phR9[ph] < 0.94 && eventClass == 3) ||
+      (preselTree->phR9[ph] < 0.298 && eventClass == 4) ||
+      (preselTree->phR9[ph] < 0.94 && eventClass == 5) ||
+      (preselTree->phR9[ph] < 0.24 && eventClass == 6) )
+    return 0;
+  if(gFlagDebug) printf("PASSED\n");
+
+  // matching
+  if(preselTree->_flagMC && preselTree->phMatch[ph] > 0.1)
+    return 0;
+
+  // all cuts passed: return true
+  return 1;
+}
+
 // routine for electron-muon pair selection
 // (select best e-mu pair in the event, with highest pT)
 // Arguments:
@@ -105,7 +164,7 @@ double SelectPh(const int eventClass, const ZTree* preselTree, const int ph)
 //   TLorentzVector& vecLepP: selected lepton+ (output)
 //   double& maxPtDiLep: transverse momentum of the selected dilepton pair (output)
 // If no dilepton pair is selected, maxPtDiLep remains unchanged 
-int SelectHgg(const ZTree* preselTree, int reqEventClass, TLorentzVector& momPh1, TLorentzVector& momPh2)
+int SelectHgg(const ZTree* preselTree, int reqEventClass, TLorentzVector& momPh1, TLorentzVector& momPh2, TString name)
 {
   int phClass[2];
   double phR9[2];
@@ -116,24 +175,46 @@ int SelectHgg(const ZTree* preselTree, int reqEventClass, TLorentzVector& momPh1
   // double loop over photons
   for(int ph1 = 0; ph1 < preselTree->Nph; ph1++)
   {
+	//photon position 
     if(TMath::Abs(preselTree->phEta[ph1]) > 2.5)
       return 0;
     if(TMath::Abs(preselTree->phEta[ph1]) > 1.44 && TMath::Abs(preselTree->phEta[ph1]) < 1.57)
       return 0;
+    //get Photon class
     phClass[0] = PhotonClass(preselTree->phEta[ph1], preselTree->phR9[ph1]);
-    if(!SelectPh(phClass[0], preselTree, ph1))
-      continue;
+    //PhotonID selection
+    if(name == "data2011")
+    {
+      if(!SelectPh11(phClass[0], preselTree, ph1))
+        continue;
+    }
+    if(name == "data2012")
+    {
+      if(!SelectPh12(phClass[0], preselTree, ph1))
+        continue;
+    }
     ph[0].SetPtEtaPhiM(preselTree->phPt[ph1], preselTree->phEta[ph1], preselTree->phPhi[ph1], 0.0);
 
     for(int ph2 = ph1 + 1; ph2 < preselTree->Nph; ph2++)
     {
+	  //photon position
       if(TMath::Abs(preselTree->phEta[ph2]) > 2.5)
         return 0;
       if(TMath::Abs(preselTree->phEta[ph2]) > 1.44 && TMath::Abs(preselTree->phEta[ph2]) < 1.57)
         return 0;
+      //get photon class
       phClass[1] = PhotonClass(preselTree->phEta[ph2], preselTree->phR9[ph2]);
-      if(!SelectPh(phClass[1], preselTree, ph2))
-        continue;
+      //PhotonID selection
+      if(name == "data2011")
+      {
+        if(!SelectPh11(phClass[0], preselTree, ph1))
+          continue;
+      }
+      if(name == "data2012")
+      {
+        if(!SelectPh12(phClass[0], preselTree, ph1))
+		  continue;
+      }
       ph[1].SetPtEtaPhiM(preselTree->phPt[ph2], preselTree->phEta[ph2], preselTree->phPhi[ph2], 0.0);
 
       // determine event class
