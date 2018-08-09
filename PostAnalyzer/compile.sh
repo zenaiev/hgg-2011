78e86a990fe7ed6ca76abfad082f2ce36d1f7707
christian512 <c.staufe@posteo.net>
2018-08-09 17:37:56 +0200

    added evolution of errors and chi2

--- PostAnalyzer/compile.sh
+++ PostAnalyzer/compile.sh
@@ -3,6 +3,7 @@
 # compile code (produces two executables)
 g++ -g -o hggMakeHist `root-config --cflags --libs` -lMathMore -std=c++11 hggMakeHist.cxx
 g++ -g -o hggMakePlots `root-config --cflags --libs` -std=c++11 hggMakePlots.cxx
+g++ -g -o pvalPlot `root-config --cflags --libs` -std=c++11 pvalPlot.cxx
 
 # create needed directories if do not exist yet
 mkdir -p data mc hist plots
--- PostAnalyzer/pvalPlot.cxx
+++ PostAnalyzer/pvalPlot.cxx
@@ -22,29 +22,14 @@
 #include "TPaveText.h"
 #include "TMath.h"
 
-double ChiSquared(int intMin, int intMax, double fitMin = 100.0, double fitMax = 180.0){
-	//import data
-	printf("intmin : %d \n", intMin);
-	printf("intMax : %d \n", intMax);
-	TString baseDir = gHistDir; //from settings.h
-	TFile* f_data11 = TFile::Open(TString::Format("%s/data2011_10GeV.root",baseDir.Data()));
-	TFile* f_data12 = TFile::Open(TString::Format("%s/data2012_10GeV.root",baseDir.Data()));
-	TH1D* h_data11 = (TH1D*) f_data11->Get(TString::Format("h_mgg1"));
-	TH1D* h_data = (TH1D*) f_data12->Get(TString::Format("h_mgg1"));
-	h_data->Add(h_data11); //h_data holds all events
-	//perform background fit
-	TF1* fit_b = new TF1("fit_b","pol5",fitMin,fitMax);
-	TFitResultPtr fitRes = h_data->Fit(fit_b,"IEMR","same",fitMin,fitMax);
-
+double ChiSquared(int intMin, int intMax, TF1* fit_b,TH1D* h_data){
 	//set the range for chi2
 	double xmin = h_data->GetXaxis()->GetBinCenter(1)-0.5;
-	
 	//calculate the chi2
 	double chi2 = 0.0;
-	printf("Fit done \n");
 	for(int i = intMin; i < intMax; i++)
 	{
-		double intError = fit_b->IntegralError(i-xmin,i-xmin+1); //slow!
+		double intError = fit_b->IntegralError(i,i+1);
 		int binNum = (int) i - xmin + 1;
 		double data = h_data->GetBinContent(binNum);
 		double dataErr = h_data->GetBinError(binNum);
@@ -55,7 +40,15 @@ double ChiSquared(int intMin, int intMax, double fitMin = 100.0, double fitMax =
 	return chi2;
 }
 
-double ChiSquaredFits(double m_H,int intMin,int intMax,double fitMin = 100.0, double fitMax = 180.0)
+
+//calculate the pValue for given degrees of freedom and chi2
+double pValue(int ndf, double chi2)
+{
+	return TMath::Prob(chi2,ndf);
+}
+
+//plot integral error for several ranges
+int chi2Evol()
 {
 	TString baseDir = gHistDir; //from settings.h
 	TFile* f_data11 = TFile::Open(TString::Format("%s/data2011_10GeV.root",baseDir.Data()));
@@ -64,38 +57,68 @@ double ChiSquaredFits(double m_H,int intMin,int intMax,double fitMin = 100.0, do
 	TH1D* h_data = (TH1D*) f_data12->Get(TString::Format("h_mgg1"));
 	h_data->Add(h_data11); //h_data holds all events
 	//perform background fit
-	TF1* fit_b = new TF1("fit_b","pol5",fitMin,fitMax);
-	TFitResultPtr fitRes_b = h_data->Fit(fit_b,"IEMR","same",fitMin,fitMax);
-	//perform background + signal fit
-	TF1* fit_sb = new TF1("fit_sb","gaus(0)+pol5(3)",fitMin,fitMax);
-	//set start parameters
-	fit_sb->SetParameter(0, 0.0);
-	fit_sb->SetParameter(1, m_H);
-	fit_sb->SetParameter(2, 2.0);
-	fit_sb->FixParameter(1, m_H); // fix the Higgs mass
-	//set initial parameters for background
-	for(int p = 3; p <= 9; p++)
-		fit_sb->SetParameter(p,fit_b->GetParameter(p-3));
-	//fit
-	TFitResultPtr fitRes_sb = h_data->Fit(fit_sb,"IEMR","same",fitMin,fitMax);
+	TF1* fit_b = new TF1("fit_b","pol5",100.0,180.0);
+	TFitResultPtr fitRes_b = h_data->Fit(fit_b,"IEMR","same",100.0,180.0);
+	//get minimum x value
 	double xmin = h_data->GetXaxis()->GetBinCenter(1)-0.5;
-	//calculate chi2
-	double chi2 = 0.0;
-	for(int i = intMin; i < intMax; i++)
+	//maximum range to each side
+	int r_max = 25;
+	double r_list[r_max],chi2_list[r_max];
+	//loop over several ranges around 125GeV
+	for(int r = 1; r <= r_max;r++)
 	{
-		int binNum = (int) i - xmin +1;
-		double bVal = fit_b->Eval(h_data->GetXaxis()->GetBinCenter(binNum));;
-		double sbVal = fit_sb->Eval(h_data->GetXaxis()->GetBinCenter(binNum));;
-		chi2 += (sbVal - bVal)*(sbVal - bVal)/bVal;
+		r_list[r-1] = r;
+		chi2_list[r-1] = ChiSquared(125-r,125+r,fit_b,h_data);
 	}
-	return chi2;
 
+	//create new TGraph
+	TCanvas *c1 = new TCanvas("c1","Background integral error",600,600);
+	TGraph *gr = new TGraph(r_max,r_list,chi2_list);
+	gr->SetTitle("Chi_Squared");
+	gr->GetXaxis()->SetTitle("Width of range (around 125 GeV)");
+	gr->SetMarkerStyle(7);
+	gr->SetMarkerColor(kRed);
+	gr->Draw("ap");
+	c1->SaveAs("plots/chi2.pdf");
+	return 1;
 }
 
-//calculate the pValue for given degrees of freedom and chi2
-double pValue(int ndf, double chi2)
+int ErrEvol()
 {
-	return TMath::Prob(chi2,ndf);
+	TString baseDir = gHistDir; //from settings.h
+	TFile* f_data11 = TFile::Open(TString::Format("%s/data2011_10GeV.root",baseDir.Data()));
+	TFile* f_data12 = TFile::Open(TString::Format("%s/data2012_10GeV.root",baseDir.Data()));
+	TH1D* h_data11 = (TH1D*) f_data11->Get(TString::Format("h_mgg1"));
+	TH1D* h_data = (TH1D*) f_data12->Get(TString::Format("h_mgg1"));
+	h_data->Add(h_data11); //h_data holds all events
+	//perform background fit
+	TF1* fit_b = new TF1("fit_b","pol5",100.0,180.0);
+	TFitResultPtr fitRes_b = h_data->Fit(fit_b,"IEMR","same",100.0,180.0);
+	//get minimum x value
+	double xmin = h_data->GetXaxis()->GetBinCenter(1)-0.5;
+	//maximum range to each side
+	int r_max = 25;
+	double r_list[r_max],intErr_list[r_max];
+	//loop over several ranges around 125GeV
+	for(int r = 1; r <= r_max;r++)
+	{
+		r_list[r-1] = r;
+		double intErr = 0.0;
+		for(int i = -r; i < r; i++)
+			intErr += fit_b->IntegralError(125+i,125+i+1);
+		intErr_list[r] = intErr;
+	}
+
+	//create new TGraph
+	TCanvas *c1 = new TCanvas("c1","Background integral error",600,600);
+	TGraph *gr = new TGraph(r_max,r_list,intErr_list);
+	gr->SetTitle("Background error");
+	gr->GetXaxis()->SetTitle("Width of range (around 125 GeV)");
+	gr->SetMarkerStyle(7);
+	gr->SetMarkerColor(kRed);
+	gr->Draw("ap");
+	c1->SaveAs("plots/intErr.pdf");
+	return 1;
 }
 
 
@@ -103,10 +126,9 @@ double pValue(int ndf, double chi2)
 //***********Main Function*************
 int main(int argc, char** argv)
 {
-	printf("p-value plot \n");
-	double chi2 = ChiSquared(124,126);
-	printf("chi2 : %f \n", chi2);
-	int ndf = 80-9;
-	double p = pValue(chi2,ndf);
-	printf("p-val : %f \n", p);
+	//int ndf = 80-9;
+	//double p = pValue(chi2,ndf);
+	//("p-val : %f \n", p);
+	chi2Evol();
+	ErrEvol();
 }
